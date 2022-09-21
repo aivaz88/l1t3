dc4574fbb2f330dde0f0c92a7da64a0792591fb6
aivaz88 <aivaz1988@mail.ru>
2022-09-22 00:20:31 +0500

    comment

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0){
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет");
+}
